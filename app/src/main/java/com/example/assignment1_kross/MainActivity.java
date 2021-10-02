ab4f4b26589212c3dc3f09ba498920925e090a4b
Kim Ross <kim.ross@lz95.org>
2021-10-02 15:49:14 -0500

    Saving Progress

--- app/src/main/java/com/example/assignment1_kross/MainActivity.java
+++ app/src/main/java/com/example/assignment1_kross/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.assignment1_kross;
 
+import static android.widget.Toast.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
-    String TAG = "";
+    private static final String TAG = "MainActivity";
     int tipPercent = 0;
+
+    //TextView output1 = findViewById(R.id.textViewWhatever)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +28,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void rdoGroupTipPercent_click(View v) {
         //((RadioButton)v).setChecked(false) - unselects the radio if the bill is empty
-
-        if (v.getId() == R.id.radioButton) {
-            tipPercent = 5;
-        }else if (v.getId() == R.id.radioButton2) {
-            tipPercent = 10;
-        }else if (v.getId() == R.id.radioButton3) {
+        Log.d(TAG, "-----------HEY!!!!!!!!__________");
+        if (v.getId() == R.id.radioButton12) {
+            tipPercent = 12;
+        }else if (v.getId() == R.id.radioButton15) {
             tipPercent = 15;
-        }else if (v.getId() == R.id.radioButton) {
-            tipPercent = 25;
+        }else if (v.getId() == R.id.radioButton18) {
+            tipPercent = 18;
+        }else if (v.getId() == R.id.radioButton20) {
+            tipPercent = 20;
         }else {
             String label = (String) ((RadioButton)v).getText();
             //Log.d(TAG, "TipPercent:  Unexpected Tip Percent Selected");
         }
+        makeText(this, String.format("Radio was selected: %2d", tipPercent), LENGTH_SHORT).show();
+        Log.d(TAG, String.format("Selected radio %d",tipPercent ));
     }
+    public void calculateAmounts() {
+        EditText txtTotal = findViewById(R.id.txtTotal);
+        TextView calcTipAmount = findViewById(R.id.calcTipAmount);
+        TextView calcTotalWithTip = findViewById(R.id.calcTotalWTip);
+        EditText txtNumPeople = findViewById(R.id.txtPeople);
+        TextView calcTotalPerPerson = findViewById(R.id.calctotalPerPerson);
+        TextView calcOverage = findViewById(R.id.calcOverage);
+
+
+    }
+    //private get
 }
